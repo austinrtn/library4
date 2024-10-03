f7472970becffca6a7ac597601c46978a59c8422
Austin Kolongowski <45217700+austinrtn@users.noreply.github.com>
2024-10-03 17:51:39 -0400

    Add files via upload

---
+++ install-library.bat
@@ -0,0 +1,21 @@
+@echo off
+SET url=https://github.com/austinrtn/library4/archive/refs/heads/main.zip
+
+SET /p dir=DIRECTORY: 
+
+IF NOT EXIST %dir% (mkdir %dir%)
+
+cd %dir%
+
+wget %url%
+
+powershell Expand-Archive main.zip
+
+del "main.zip"
+
+robocopy "main/library4-main/" "%cd%" /E /DCOPY:DAT /COPY:DAT /R:5 /W:10
+
+rmdir /s /q main
+
+del "install-library.bat"
+
