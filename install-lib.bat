18454ff7e628986473558c3cffb36bba6cab5008
austinrtn <kolowemployment@gmail.com>
2024-10-04 12:55:41 -0400

    bat changes

--- install-lib.bat
+++ install-lib.bat
@@ -1,13 +1,31 @@
 @echo off
 SET url=https://github.com/austinrtn/library4/archive/refs/heads/main.zip
 
+ECHO pinging github...
+ping -n 1 github.com >nul
+if errorlevel 1 (
+echo Failure to connect to: github.com
+pause
+exit
+) else ECHO success 
+ECHO . 
+ECHO .
+
 SET /p dir=DIRECTORY: 
 
 IF NOT EXIST %dir% (mkdir %dir%)
 
 cd %dir%
 
+ECHO Downloading Library...
+
 wget %url%
+
+IF NOT EXIST main.zip (
+ECHO There was an issue downloading the file...
+PAUSE
+EXIT
+) ELSE ECHO Download Complete
 
 powershell Expand-Archive main.zip
 
@@ -18,4 +36,6 @@ robocopy "main/library4-main/" "%cd%" /E /DCOPY:DAT /COPY:DAT /R:5 /W:10
 rmdir /s /q main
 
 del "install-lib.bat"
+
+start https://localhost/%dir%/
 
--- update-lib.bat
+++ update-lib.bat
@@ -1,9 +1,27 @@
 @echo off
 
 set url=https://github.com/austinrtn/library4/archive/refs/heads/main.zip
+
+ECHO pinging github...
+ping -n 1 github.com >nul
+if errorlevel 1 (
+echo Failure to connect to: github.com
+pause
+exit
+) else ECHO success 
+ECHO . 
+ECHO .
+
 rmdir library /s /q
 
 wget %url%
+
+IF NOT EXIST main.zip (
+ECHO There was an issue downloading the file...
+PAUSE
+EXIT
+) ELSE ECHO Download Complete
+
 powershell Expand-Archive main.zip
 
 del "main.zip"
@@ -13,3 +31,4 @@ robocopy "main/library4-main/library" "library" /E /DCOPY:DAT /COPY:DAT /R:5 /W:
 rmdir /s /q main
 
 echo Library Updated!
+
