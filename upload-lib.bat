fbe5591e9415acd78d84cbf6e969abf1fd48525b
Austin Kolongowski <45217700+austinrtn@users.noreply.github.com>
2024-10-04 13:00:24 -0400

    Update upload-lib.bat

--- upload-lib.bat
+++ upload-lib.bat
@@ -4,7 +4,7 @@ SET source=%cd%\library
 SET defaultTarget=C:\xampp\htdocs\library4
 SET url=https://github.com/austinrtn/library4/archive/refs/heads/main.zip
 
-SET /p ans=Are you sure you want to commit changes to 'library'? [Y/N]
+SET /p ans=Are you sure you want to commit changes to 'library'? [y/n]
 
 IF NOT %ans%==y exit
 
