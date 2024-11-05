ffcef6d95cd5c56e266e0e2ce375748dc2d1d6a2
austinrtn <kolowemployment@gmail.com>
2024-11-04 19:58:41 -0500

    Uploaded from C:\xampp\htdocs\coltect-demo\library

--- library/physics/collision_detection/classicCollisionDetection.js
+++ library/physics/collision_detection/classicCollisionDetection.js
@@ -1,4 +1,5 @@
-import { squareIntersects } from "../../utils.js";
+import { getDistance } from "../../utils.js";
+
 let collisionType = "";
 let collisionObjs = [];
 
@@ -37,8 +38,10 @@ export function update(){
             let obj2 = collisionObjs[j];
             
             let colCheck;
-            if(collisionType == "rectangle") colCheck = squareIntersects(obj1, obj2);
-            else if(collisionType == "circle") colCheck = circleCollisionCheck(obj1,obj2);
+            if(obj1.shape == 'rectangle' && obj2.shape == 'rectangle') colCheck = rectangleCollisionCheck(obj1, obj2);
+            else if(obj1.shape == 'circle' && obj2.shape == 'circle') colCheck =circleCollisionCheck(obj1, obj2);
+            else if((obj1.shape == 'rectangle' && obj2.shape == 'circle') || 
+                        obj1.shape == 'circle' && obj2.shape == 'rectangle') colCheck =rectCircleCollisionCheck(obj1, obj2);
             
             if(colCheck) obj1.onCollision(obj2);
           
@@ -53,6 +56,15 @@ export function update(){
     }
 }
 
+export function rectangleCollisionCheck(obj1, obj2){
+    return !(
+        obj1.x > obj2.x + obj2.width ||
+        obj1.x + obj1.width < obj2.x ||
+        obj1.y > obj2.y + obj2.height || 
+        obj1.y + obj1.height < obj2.y
+    );
+}
+
 function circleCollisionCheck(obj1,obj2){
     let a = obj1.x - obj2.x;
     let b = obj1.y - obj2.y;
@@ -60,4 +72,34 @@ function circleCollisionCheck(obj1,obj2){
 
     let radii = obj1.r + obj2.r;
     return radii >= c;
+}
+
+function rectCircleCollisionCheck(obj1, obj2){
+    let rect;
+    let circle; 
+
+    if(obj1.shape == 'rectangle') {
+        rect = obj1;
+        circle = obj2;
+    } else {
+        rect = obj2;
+        circle = obj1;
+    }
+
+    
+    let distance = getDistance(circle, rect.getCenter());
+    let distX = Math.abs(distance.x);
+    let distY = Math.abs(distance.y);
+    let cornerDist;
+
+    if(distX > (rect.width/2 + circle.r)) return false;
+    if(distY > (rect.height/2 + circle.r)) return false;
+
+    if(distX <= rect.width/2) return true;
+    if(distY <= rect.height/2) return true; 
+
+    cornerDist = Math.sqrt(distX - rect.width/2);
+    cornerDist += Math.sqrt(distY - rect.height/2);
+
+    return (cornerDist <= ((circle.r/2)));
 }
--- library/utils.js
+++ library/utils.js
@@ -56,14 +56,6 @@ export function containsPointInSquare(obj, point){
     );
 }
 
-export function squareIntersects(obj1, obj2){
-    return !(
-        obj1.x > obj2.x + obj2.width ||
-        obj1.x + obj1.width < obj2.x ||
-        obj1.y > obj2.y + obj2.height || 
-        obj1.y + obj1.height < obj2.y
-    );
-}
 
 export function containsPointInCircle(obj, point){
     let d = Math.pow((point.x - this.x), 2) + Math.pow((point.y - this.y), 2);
